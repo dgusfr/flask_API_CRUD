86f1ba46625c8abdeb339f9179e774b9528b9f36
dgusfr <diegogustavo.fr@gmail.com>
2024-12-31 09:01:02 -0300

    feat(service): adicionar serviço para escrita no JSON

--- services/json_service.py
+++ services/json_service.py
@@ -3,3 +3,7 @@ import json
 def read_json():
     with open('database.json', 'r') as file:
         return json.load(file)
+
+def write_json(data):
+    with open('database.json', 'w') as file:
+        json.dump(data, file, indent=4)
