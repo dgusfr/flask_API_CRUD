30060499351f29eaa351215511821b6f82f34a23
dgusfr <diegogustavo.fr@gmail.com>
2024-12-31 08:59:38 -0300

    feat(controller): implementar controlador para criar itens

--- controllers/item_controller.py
+++ controllers/item_controller.py
@@ -1,6 +1,10 @@
-from flask import jsonify
-from services.json_service import read_json
+from flask import request, jsonify
+from services.json_service import read_json, write_json
 
-def get_items():
+def create_item():
     data = read_json()
-    return jsonify(data['items'])
+    new_item = request.json
+    new_item['id'] = len(data['items']) + 1
+    data['items'].append(new_item)
+    write_json(data)
+    return jsonify(new_item), 201
