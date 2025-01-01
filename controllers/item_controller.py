86c7ccff815ee6f8fa1270a6ec469b1ccf705b13
dgusfr <diegogustavo.fr@gmail.com>
2025-01-01 09:25:21 -0300

    refactor(controller): ajustar controlador para listar itens a partir do MySQL

--- controllers/item_controller.py
+++ controllers/item_controller.py
@@ -1,30 +1,5 @@
-from flask import request, jsonify
-from services.json_service import read_json, write_json
+from models.item import Item
 
 def get_items():
-    data = read_json()
-    return jsonify(data['items'])
-
-def create_item():
-    data = read_json()
-    new_item = request.json
-    new_item['id'] = len(data['items']) + 1
-    data['items'].append(new_item)
-    write_json(data)
-    return jsonify(new_item), 201
-
-def update_item(item_id):
-    data = read_json()
-    updated_item = request.json
-    for item in data['items']:
-        if item['id'] == item_id:
-            item.update(updated_item)
-            write_json(data)
-            return jsonify(item)
-    return jsonify({'error': 'Item not found'}), 404
-
-def delete_item(item_id):
-    data = read_json()
-    data['items'] = [item for item in data['items'] if item['id'] != item_id]
-    write_json(data)
-    return jsonify({'message': 'Item deleted'}), 200
+    items = Item.query.all()
+    return jsonify([{'id': item.id, 'name': item.name} for item in items])
