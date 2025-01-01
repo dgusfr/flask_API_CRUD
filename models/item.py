91df07f6c51951f2669a12576c11b4ca8d22bd82
dgusfr <diegogustavo.fr@gmail.com>
2025-01-01 09:24:06 -0300

    feat(models): criar modelo de tabela para itens

---
+++ models/item.py
@@ -0,0 +1,6 @@
+from config.database import db
+
+class Item(db.Model):
+    __tablename__ = 'items'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(80), nullable=False)
