bfb022b8711e4b4983bd631c702db1ef63bdeb59
dgusfr <diegogustavo.fr@gmail.com>
2025-01-01 09:25:33 -0300

    refactor(controller): ajustar criação de itens para utilizar MySQL

--- controllers/item_controller.py
+++ controllers/item_controller.py
@@ -3,3 +3,10 @@ from models.item import Item
 def get_items():
     items = Item.query.all()
     return jsonify([{'id': item.id, 'name': item.name} for item in items])
+
+def create_item():
+    data = request.json
+    new_item = Item(name=data['name'])
+    db.session.add(new_item)
+    db.session.commit()
+    return jsonify({'id': new_item.id, 'name': new_item.name}), 201
