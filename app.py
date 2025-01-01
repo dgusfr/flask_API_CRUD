c03f86a166bf1b9101fcd44227f89f6b120aab8e
dgusfr <diegogustavo.fr@gmail.com>
2025-01-01 09:28:07 -0300

    feta update

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from routes.item_routes import item_routes
 from config.database import init_db
 from config.migrate import init_migrate
 
-# Inicializar aplicação Flask
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@localhost/database_name'
@@ -15,6 +14,5 @@ migrate = init_migrate(app)
 
 app.register_blueprint(item_routes)
 
-# Ponto de entrada da aplicação
 if __name__ == '__main__':
     app.run(debug=True)
