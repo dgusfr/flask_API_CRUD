6a0bf9636d09d60f42aac311b4dda48ec5b0338e
dgusfr <diegogustavo.fr@gmail.com>
2025-01-01 09:31:49 -0300

    feta update

--- config/migrate.py
+++ config/migrate.py
@@ -2,5 +2,11 @@ from flask_migrate import Migrate
 from config.database import db
 
 def init_migrate(app):
+    """
+    Inicializa o Flask-Migrate com o aplicativo Flask.
+    
+    :param app: Instância do aplicativo Flask
+    :return: Instância do Migrate
+    """
     migrate = Migrate(app, db)
     return migrate
