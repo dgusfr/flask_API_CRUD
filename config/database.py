edbd1a82caefda6d9aea7edf90bcca55b507eda7
dgusfr <diegogustavo.fr@gmail.com>
2025-01-01 09:39:25 -0300

    feat update

--- config/database.py
+++ config/database.py
@@ -3,6 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@localhost/database_name'
+    """
+    Configura a conexão com o banco de dados MySQL e inicializa o SQLAlchemy.
+    
+    :param app: Instância do aplicativo Flask
+    """
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://diego:1234@localhost/flask_crud_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
