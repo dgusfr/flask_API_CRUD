84222347d0710541ee6ada4b9fe15581abc7be2c
dgusfr <diegogustavo.fr@gmail.com>
2025-01-02 10:08:55 -0300

    feat(config): configurar Flask-Migrate para gerenciar migrações

--- config/migrate.py
+++ config/migrate.py
@@ -2,11 +2,4 @@ from flask_migrate import Migrate
 from config.database import db
 
 def init_migrate(app):
-    """
-    Inicializa o Flask-Migrate com o aplicativo Flask.
-    
-    :param app: Instância do aplicativo Flask
-    :return: Instância do Migrate
-    """
-    migrate = Migrate(app, db)
-    return migrate
+    return Migrate(app, db)
