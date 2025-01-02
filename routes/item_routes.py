908d43da1cad7e71bea5d84231ae5a21b5985071
dgusfr <diegogustavo.fr@gmail.com>
2025-01-02 10:12:42 -0300

    feat remove

--- routes/item_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from controllers.item_controller import get_items, create_item, update_item, delete_item
-
-item_routes = Blueprint('item_routes', __name__)
-
-item_routes.route('/items', methods=['GET'])(get_items)
-item_routes.route('/items', methods=['POST'])(create_item)
-item_routes.route('/items/<int:item_id>', methods=['PUT'])(update_item)
-item_routes.route('/items/<int:item_id>', methods=['DELETE'])(delete_item)
