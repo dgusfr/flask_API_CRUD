14a44ea9e079de9ded0e8b15ed6f04b49be46b89
dgusfr <diegogustavo.fr@gmail.com>
2025-01-02 10:08:27 -0300

    feat(app): configurar e inicializar a aplicação principal com rotas de produtos

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
 from flask import Flask
-from routes.item_routes import item_routes
 from config.database import init_db
 from config.migrate import init_migrate
+from routes.product_routes import product_routes
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@localhost/database_name'
+# Configuração do banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask_user:1234@localhost/flask_crud_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Inicialização do banco de dados e migrações
 init_db(app)
-
 migrate = init_migrate(app)
 
-app.register_blueprint(item_routes)
-
-@app.route('/')
-def home():
-    return "API Flask funcionando! Acesse /items para listar os itens."
+# Registro das rotas
+app.register_blueprint(product_routes)
 
 if __name__ == '__main__':
     app.run(debug=True)
