1c1bde3d0ae478bf31bcbae5cf6eff9f434432b0
dgusfr <diegogustavo.fr@gmail.com>
2025-01-02 10:23:40 -0300

    feat update

--- test_api.py
+++ test_api.py
@@ -2,3 +2,11 @@ import requests
 
 URL = "http://127.0.0.1:5000/products"
 
+def list_products():
+  response = requests.get(URL)
+  if response.status == 200:
+    print("Produtos disponiveis")
+    for product in  response.json():
+      print(f"- ID: {product['id']}, Nome: {product['name']}, Estoque: {product['stock']}")
+    else:
+      print("Erro ao listar produtos:", response.status_code, response.text)
