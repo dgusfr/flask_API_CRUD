e47ad99e9e0f77271d9b15b331cf154336cd9c3e
dgusfr <diegogustavo.fr@gmail.com>
2025-01-02 10:32:32 -0300

    feat update

--- test_api.py
+++ test_api.py
@@ -3,13 +3,18 @@ import requests
 URL = "http://127.0.0.1:5000/products"
 
 def list_products():
-  response = requests.get(URL)
-  if response.status == 200:
-    print("Produtos disponiveis")
-    for product in  response.json():
-      print(f"- ID: {product['id']}, Nome: {product['name']}, Estoque: {product['stock']}")
+    response = requests.get(BASE_URL)
+    if response.status_code == 200:
+        products = response.json()
+        if not products:
+            print("Nenhum produto disponível.")
+        else:
+            print("Produtos disponíveis:")
+            for product in products:
+                print(f"- ID: {product['id']}, Nome: {product['name']}, Estoque: {product['stock']}")
     else:
-      print("Erro ao listar produtos:", response.status_code, response.text)
+        print("Erro ao listar produtos:", response.status_code, response.text)
+
 
 def create_product(name, description, price, stock):
     payload = {
