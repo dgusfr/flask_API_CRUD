4c0c01e0a784d614c3a0ade9e8347683a6329d79
dgusfr <diegogustavo.fr@gmail.com>
2025-01-02 10:12:15 -0300

    feat remove

--- models/item.py
+++
@@ -1,6 +0,0 @@
-from config.database import db
-
-class Item(db.Model):
-    __tablename__ = 'items'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(80), nullable=False)
