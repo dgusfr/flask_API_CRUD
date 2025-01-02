d076845e2d494d624d1268bc168ce110a4354178
dgusfr <diegogustavo.fr@gmail.com>
2025-01-02 10:30:58 -0300

    feat update

--- routes/product_routes.py
+++ routes/product_routes.py
@@ -4,6 +4,7 @@ from controllers.product_controller import ProductController
 product_routes = Blueprint('product_routes', __name__)
 
 product_routes.route('/products', methods=['GET'])(ProductController.get_products)
+product_routes.route('/products/<int:product_id>', methods=['GET'])(ProductController.get_product_by_id)
 product_routes.route('/products', methods=['POST'])(ProductController.create_product)
 product_routes.route('/products/<int:product_id>', methods=['PUT'])(ProductController.update_product)
 product_routes.route('/products/<int:product_id>', methods=['DELETE'])(ProductController.delete_product)
