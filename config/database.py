736715d746ef7c72f949dcc70c72015583c9a8eb
dgusfr <diegogustavo.fr@gmail.com>
2025-01-02 10:08:42 -0300

    feat(config): adicionar configuração inicial para conexão ao banco de dados MySQL

--- config/database.py
+++ config/database.py
@@ -3,11 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    """
-    Configura a conexão com o banco de dados MySQL e inicializa o SQLAlchemy.
-    
-    :param app: Instância do aplicativo Flask
-    """
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://diego:1234@localhost/flask_crud_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask_user:1234@localhost/flask_crud_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
