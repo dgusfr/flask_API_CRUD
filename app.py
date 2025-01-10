585ee7401de2dce8c67c713e3fff4e0c5b67a5e3
dgusfr <diegogustavo.fr@gmail.com>
2025-01-10 19:31:57 -0300

    fix(app): update

--- app.py
+++ app.py
@@ -4,16 +4,12 @@ from config.migrate import init_migrate
 from routes.product_routes import product_routes
 
 app = Flask(__name__)
-
-# Configuração do banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask_user:1234@localhost/flask_crud_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Inicialização do banco de dados e migrações
 init_db(app)
 migrate = init_migrate(app)
 
-# Registro das rotas
 app.register_blueprint(product_routes)
 
 if __name__ == '__main__':
